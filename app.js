9701e7df11ef2fde4586c09f85dd1c7d02fbf6f1
Josh Eardley <josheardley@gmail.com>
2023-04-18 08:37:16 -0600

    Posts route added

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const morgan = require("morgan");
+const { list, find } = require("./postBank");
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
 const PORT = 1337;
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  const posts = list();
+
+  const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+    <head>
+    <body>
+      <ul>
+        ${posts.map(post => `<li>Title: ${post.title}<br>Author: ${post.name}</li>`)}
+      </ul>
+    </body>
+    </html>`;
+
+    res.send(html);
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
